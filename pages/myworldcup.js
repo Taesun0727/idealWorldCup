c3469025f9bced4ec29ba7a52eb26a10f084f77e
Taesun <xotjs5125@gmail.com>
2022-03-25 15:59:44 +0900

    기능추가: 내가 만든 월드컵 화면 변경

--- pages/myworldcup.js
+++ pages/myworldcup.js
@@ -3,6 +3,7 @@ import noimg from "../assets/img/noimage.jpg"
 import styled from "styled-components"
 import { AiOutlineSearch, AiFillCaretRight, AiFillEdit, AiOutlineRise } from 'react-icons/ai'
 import { BsFillTrashFill } from 'react-icons/bs'
+import Link from "next/link"
 
 const Title = styled.label`
   display: block;
@@ -33,22 +34,37 @@ const WorldcupCreateButton = styled(Button)`
 
 const StyleInput = styled.input`
   height: 25px;
+  width: 70%;
+`;
+
+const StyleLink = styled(Link)`
+  text-decoration: none;
+
+  &:focus, &:hover, &:visited, &:link, &:active {
+    text-decoration: none;
+}
 `;
 
 const myworldcup = () => {
   return (
     <AppLayout>
       <MyworldcupHeader>
-        <Button>인기순</Button>
-        <Button>최신순</Button>
-        <StyleInput/>
-        <Button><AiOutlineSearch/></Button>
-        <WorldcupCreateButton>내 이상형 월드컵 만들기</WorldcupCreateButton>
+        <div style={{display: "inline-block", width: "20%", textAlign: "end"}}>
+          <Button>인기순</Button>
+          <Button>최신순</Button>
+        </div>
+        <div style={{display: "inline-block", width: "50%"}}>
+          <StyleInput/>
+          <Button><AiOutlineSearch/></Button>
+        </div>
+        <div style={{display: "inline-block", width: "30%"}}>
+          <StyleLink href='/'><WorldcupCreateButton>내 이상형 월드컵 만들기</WorldcupCreateButton></StyleLink>
+        </div>
       </MyworldcupHeader>
       <MyworldcupBody>
-        <div style={{display: "inline-block"}}>
+        <div style={{display: "inline-block", width: "22%"}}>
           <div>
-            <img src={noimg.src}></img>
+            <img src={noimg.src} style={{width: "100%"}}></img>
           </div>
           <div style={{background: "white"}}>
             <Title>타이틀</Title>
@@ -61,9 +77,39 @@ const myworldcup = () => {
             </div>
           </div>
         </div>
-        <div style={{display: "inline-block"}}>
+        <div style={{display: "inline-block", width: "22%"}}>
           <div>
-            <img src={noimg.src}></img>
+            <img src={noimg.src} style={{width: "100%"}}></img>
+          </div>
+          <div style={{background: "white"}}>
+            <Title>타이틀</Title>
+            <label>내용</label>
+            <div>
+              <button><AiFillCaretRight/>시작하기</button>
+              <button><AiOutlineRise/>공유하기</button>
+              <button><AiFillEdit/>수정</button>
+              <button><BsFillTrashFill/>삭제</button>
+            </div>
+          </div>
+        </div>
+        <div style={{display: "inline-block", width: "22%"}}>
+          <div>
+            <img src={noimg.src} style={{width: "100%"}}></img>
+          </div>
+          <div style={{background: "white"}}>
+            <Title>타이틀</Title>
+            <label>내용</label>
+            <div>
+              <button><AiFillCaretRight/>시작하기</button>
+              <button><AiOutlineRise/>공유하기</button>
+              <button><AiFillEdit/>수정</button>
+              <button><BsFillTrashFill/>삭제</button>
+            </div>
+          </div>
+        </div>
+        <div style={{display: "inline-block", width: "22%"}}>
+          <div>
+            <img src={noimg.src} style={{width: "100%"}}></img>
           </div>
           <div style={{background: "white"}}>
             <Title>타이틀</Title>
